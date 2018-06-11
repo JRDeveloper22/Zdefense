7ba40f1eae6d42db936e284d194922ac73747213
ReapingShadow <dannyoflo@gmail.com>
2018-06-10 23:27:15 -0500

    AddedAnimations
    Added the animations and guns to player characther and basic movement

--- Assets/AiStateMachine/Player.cs
+++ Assets/AiStateMachine/Player.cs
@@ -4,17 +4,58 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    private Animator playerAnim = null;
+    private float speed = 1.0f;
+
+    public GameObject ACW_R = null;
+    public GameObject M4A1 = null;
+
+
+
+	void Start ()
+    {
+        playerAnim = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+        M4A1.SetActive(false);
+        ACW_R.SetActive(true);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         if (Input.GetKeyDown(KeyCode.W))
+            playerAnim.SetFloat("Speed", speed);
+        else if (Input.GetKeyUp(KeyCode.W))
+            playerAnim.SetFloat("Speed", 0.0f);
+        else if (Input.GetKeyDown(KeyCode.R))
         {
-            Debug.Log("testarino");
-        };
+            playerAnim.SetTrigger("Reloading");
+            Debug.Log("I am reloading");
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("I jumped");
+            playerAnim.SetTrigger("Jump");
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            SwitchActive();
+            Debug.Log("I am Switching Guns");
+        }
+
+        ;
 	}
+
+    void SwitchActive()
+    {
+        if (M4A1.activeSelf == true)
+        {
+            M4A1.SetActive(false);
+            ACW_R.SetActive(true);
+        }
+        else if (ACW_R.activeSelf == true)
+        {
+            M4A1.SetActive(true);
+            ACW_R.SetActive(false);
+        }
+    }
 }
